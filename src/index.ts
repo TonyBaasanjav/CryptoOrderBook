063e663cc950579b5f2a6d31408b97fbd016f55e
Tony Baasanjav <tonybaasanjav@gmail.com>
2021-09-12 11:27:48 -0500

    added HEROKU

--- src/index.ts
+++ src/index.ts
@@ -3,11 +3,28 @@ import http from 'http';
 import config from './config/config';
 import express, { NextFunction, Request, Response } from "express";
 import orderBookRoute from './routes/orderBookRoute';
+import cors from 'cors';
 
 const app = express();
 
 app.use(express.urlencoded({extended: true})); 
 app.use(express.json());
+
+// ** HEROKU MIDDLEWARE ** //
+const whitelist = ['http://localhost:8080', 'https://radiant-garden-02540.herokuapp.com']
+const corsOptions = {
+  origin: function (origin: any, callback: any) {
+    console.log("** Origin of request " + origin)
+    if (whitelist.indexOf(origin) !== -1 || !origin) {
+      console.log("Origin acceptable")
+      callback(null, true)
+    } else {
+      console.log("Origin rejected")
+      callback(new Error('Not allowed by CORS'))
+    }
+  }
+}
+app.use(cors(corsOptions));
 
 app.use((request: Request, response: Response, next: NextFunction) => {
     response.header('Access-Control-Allow-Origin', '*');
@@ -30,6 +47,17 @@ app.use((request: Request, response: Response, next: NextFunction) => {
         message: error.message
     });
 });
+
+// ** HEROKU CLIENT REDIRECTION ** //
+const path = require('path');
+if (process.env.NODE_ENV === 'production') {
+// Serve any static files
+  app.use(express.static(path.join(__dirname, 'client/build')));
+// Handle React routing, return all requests to React app
+  app.get('*', function(req, res) {
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+  });
+}
 
 const httpServer = http.createServer(app);
 
